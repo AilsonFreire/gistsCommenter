a417dbcd06ea7e56200c06224caa0399ec3167c8
Ailson Freire <ailson_freire@outlook.com>
2019-11-01 16:18:15 -0300

    Fixing Ios issues and removing react-native-oauth dependence

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { BackHandler, StatusBar } from "react-native";
 // @ts-ignore
-import OAuthManager from "react-native-oauth";
 import { CLIENT_ID, CLIENT_SECRET } from "utils/Constants";
 
 const App = () => {
@@ -9,18 +8,6 @@ const App = () => {
   useEffect(() => {
 
     const backHandler = BackHandler.addEventListener("hardwareBackPress", handleBackPress);
-
-    const manager = new OAuthManager("Comment");
-    manager.configure({
-      github: {
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET
-      }
-    });
-
-    manager.authorize("github", { scopes: "gist" })
-      .then(resp => console.log("Your users ID", resp))
-      .catch(err => console.log("There was an error"));
 
     return function cleanup() {
       backHandler.remove();
