31043493ae7ad82a14b6118fb5fa231d507a3391
Ailson Freire <ailson_freire@outlook.com>
2019-11-03 10:45:11 -0300

    Open the qrCode reader int the screen

--- src/views/reader/index.tsx
+++ src/views/reader/index.tsx
@@ -1,19 +1,37 @@
+// import { SafeArea } from "./styles";
+import { SafeArea, Typography } from "@views/welcome/styles";
 import React, { useContext } from "react";
 import { StatusBar, View } from "react-native";
 import { NavigationStackOptions } from "react-navigation-stack";
 import { ThemeContext } from "styled-components";
-// import { SafeArea } from "./styles";
-import { Typography, SafeArea } from "@views/welcome/styles";
+
+import QRCodeScanner from "react-native-qrcode-scanner";
 
 const Reader = () => {
   const {
     colors: { secondaryColor },
   } = useContext(ThemeContext);
 
+  const onSuccess = async (e: any) => {
+    console.log(e.data);
+  };
+
   return (
     <SafeArea>
       <StatusBar backgroundColor={secondaryColor} barStyle="dark-content" />
       <Typography>Aponte o leitor para uma url de Gists válida.</Typography>
+
+      <QRCodeScanner
+        onRead={onSuccess}
+        showMarker={true}
+        checkAndroid6Permissions={true}
+        //  bottomContent={
+        //   <View style={styles.touchable}>
+        //    {this.state.success && (
+        //     <Text style={styles.text}>OK. Got it!</Text>
+        //    )}
+        //  }
+      />
     </SafeArea>
   );
 };
